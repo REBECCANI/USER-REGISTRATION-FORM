34108590433128249550d138563037e7aee7ca01
REBECCA NISHIMWE <94054406+REBECCANI@users.noreply.github.com>
2024-06-06 14:06:58 +0200

    Update user_db.sql

--- user_db.sql
+++ user_db.sql
@@ -1,11 +1,22 @@
-CREATE DATABASE user_db;
+CREATE DATABASE user_registration;
 
-USE user_db;
+USE user_registration;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    token VARCHAR(255) NOT NULL,
-    confirmed BOOLEAN NOT NULL DEFAULT FALSE
+    token VARCHAR(255),
+    is_confirmed BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+SELECT * from users;
+
+DESCRIBE users;
+
+ALTER TABLE users 
+ADD COLUMN fname VARCHAR(255) NOT NULL,
+ADD COLUMN lname VARCHAR(255) NOT NULL;
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Ruramira0306!';
